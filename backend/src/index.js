7596a3013c2f6fa71736167ea89c6a524c0f2374
Prashanth <spprashaant@gmail.com>
2025-04-20 21:03:30 +0530

    feat: Added authentication and added problem model

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    res.send("Hello guys, welcome to leetlab🔥");
+});
+
+app.use("/api/v1/auth", authRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+});
