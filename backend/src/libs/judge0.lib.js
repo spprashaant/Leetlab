34a4f44b2a6e70bc6444094595aa39b0e209cea6
Prashanth <spprashaant@gmail.com>
2025-05-04 15:41:29 +0530

    feat: create submission and mark as solved

--- backend/src/libs/judge0.lib.js
+++ backend/src/libs/judge0.lib.js
@@ -60,3 +60,14 @@ export const pollBatchResults = async (tokens) => {
     }
 }
 
+export function getLanguageName(languageId){
+    const LANGUAGE_NAMES = {
+        74: "TypeScript",
+        63: "JavaScript",
+        71: "Python",
+        62: "Java"
+    }
+
+    return LANGUAGE_NAMES[languageId] || "Unknown";
+}
+
